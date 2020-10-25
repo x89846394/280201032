1027cdae44f6afbed591bec352ef8a5859fd4c1e
x89846394 <cerenkarakoc@std.iyte.edu.tr>
2020-10-25 18:10:39 +0000

    .

--- lab1/example6.py
+++ lab1/example6.py
@@ -2,6 +2,6 @@ car1_velocity = 80
 car2_velocity = 70
 distance_between_cars = 490
 
-t = (490 - 150) / (80 + 70)
+t = (distance_between_cars - 150) / (car1_velocity + car2_velocity)
 
 print(f"The distance between cars will be 150km {int(t * 60)} minutes later.")
